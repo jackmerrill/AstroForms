89c2164eeb65683f6c4fa473f64abe53fafec77c
Jack Merrill <me@jackmerrill.com>
2022-07-13 16:58:21 -0500

    feat: dark mode

--- packages/demo/tailwind.config.cjs
+++ packages/demo/tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}",
     "./node_modules/astro-forms/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}",
+    "../astro-forms/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}",
   ],
   theme: {
     extend: {},
